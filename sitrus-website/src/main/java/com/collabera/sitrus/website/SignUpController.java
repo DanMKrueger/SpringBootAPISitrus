8ee05ded3a7c76c5b39b6f59f4e57dfba31285af
Dan Krueger <krugster@yahoo.com>
2019-09-24 16:59:05 -0400

    Adding files

--- sitrus-website/src/main/java/com/collabera/sitrus/website/SignUpController.java
+++ sitrus-website/src/main/java/com/collabera/sitrus/website/SignUpController.java
@@ -1,5 +1,17 @@
 package com.collabera.sitrus.website;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SignUpController {
+	
+	@GetMapping("/hello")
+	public static String sayHello() {
+		System.out.println("Here");
+		return "Hello!";
+	}
+	
+	
 
 }
