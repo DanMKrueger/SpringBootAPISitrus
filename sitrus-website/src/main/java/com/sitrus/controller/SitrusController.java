2372c2cbfda055bc5d9387c86ec1df600ebae716
Dan Krueger <krugster@yahoo.com>
2019-09-30 16:27:37 -0400

    Updating files

--- sitrus-website/src/main/java/com/sitrus/controller/SitrusController.java
+++ sitrus-website/src/main/java/com/sitrus/controller/SitrusController.java
@@ -30,7 +30,6 @@ public class SitrusController {
 	public @ResponseBody int LoginUser(@RequestBody String enteredString) throws Exception {
 		//System.out.println(enteredUser);
 		int msg = userLogin(enteredString);
-		
 		return msg;
 	}
 	
@@ -54,9 +53,15 @@ public class SitrusController {
 			allInfo.add(str);
 		}
 
-		
+		/* 
+		 * Make an array list with the entire database in it. Then, we go through it one 
+		 * User at a time and check the username and see if it is equal to the username that 
+		 * was entered (allInfo.get(0)). If it is, that means that the user exists, so
+		 * we set our int msg equal to 0, and break out of the for loop. If no user is found msg 
+		 * will be equal to 1, and when we return 1 the JavaScript file takes that as no user being
+		 * found with that user name, so that is the alert we show.
+		*/
 		ArrayList<User> allUsers = (ArrayList<User>) userRepo.findAll();
-		
 		for(int i = 0; i < allUsers.size(); i++) {
 			if(allUsers.get(i).getUsername().equals(allInfo.get(0))) {
 				enteredUser = allUsers.get(i);
@@ -67,7 +72,13 @@ public class SitrusController {
 				msg = 1;
 			}
 		}
-		
+		/*
+		 * If we get into the if statment under this comment, that means that msg was equal to 0, which
+		 * means that there is a user in our databse with the username entered on the website.
+		 * Now, we check the password. We take the entered password and compare it to the password
+		 * we have stored in the database (allInfo.get(1)). If they match, we return 0 which is our code
+		 * to show everything is correct. Otherwise, we return 1.
+		 */
 		if(msg == 0) {
 			if(enteredUser.getUserPassword().equals(allInfo.get(1))) {
 				msg = 0;
@@ -85,45 +96,53 @@ public class SitrusController {
 	public int createUser(String userString) {
 		
 		int msg = 1;
-
-		// Create a new user object, a delimiter we use to split up our string passed
-		// in, and arrays to store information
-		// as we work with it.
+		/* Create a new user object, a delimiter we use to split up our string passed
+		 * in, and arrays to store information
+		 * as we work with it.
+		*/
+		
 		User enteredUser = new User();
 		String delims = "[&]";
 		ArrayList<String> allInfo = new ArrayList<>();
 		String newStringToAdd;
 
-		// Take our string that is passed in, and split the string up based on our
-		// delimiter we set above,
-		// which is the & symbol
+		/* 
+		 * Take our string that is passed in, and split the string up based on our
+		 * delimiter we set above, which is the & symbol
+		 */
 		String[] parsedString = userString.split(delims);
 
-		// Go through our string array that has been split up into sections, and now
-		// split it up on the = symbol so we take everything after the = and push it
-		// into our array list.
+		/* Go through our string array that has been split up into sections, and now
+		 * split it up on the = symbol so we take everything after the = and push it
+		 * into our array list.
+		 */
 		for (int i = 0; i < parsedString.length; i++) {
 			String str = parsedString[i].substring(parsedString[i].indexOf("=") + 1);
 			allInfo.add(str);
 
 		}
 
-		// The emails passed in get the @ replaced with a % symbol, so to undo that we
-		// get the email adress, and we replace the % with an @ and then replace the
-		// email with
-		// the fixed email in our array list before we make our User object.
+		/* 
+		 * The emails passed in get the @ replaced with a % symbol, so to undo that we
+		 *  get the email adress, and we replace the % with an @ and then replace the 
+		 *  email with the fixed email in our array list before we make our User object.
+		 */
 		newStringToAdd = allInfo.get(4);
 		newStringToAdd = newStringToAdd.replace("%40", "@");
 		allInfo.set(4, newStringToAdd);
 		
-		// Take all the information that has been passed in, and go through it 1 by one
-		// to create our user.
-		// The information passed in will always be in the same order, so we can set our
-		// user object
-		// like we do below.
-		
+
+		/*
+		 * We take our entire database and put it into an ArrayList named allUsers. Then with that we go through
+		 * one entry at a time and check to see if the entered username(allInfo.get(0)) is equal to any of the usernames
+		 * that is already in the database. If it is, we set msg = 1 and we break because that means the username is already taken,
+		 * and when we return 1 in this method, 1 gets returned to the JS file and it knows that 1 is the code
+		 * for the uesrname already being taken. If the username is not there, we make msg = 0 which is good. If it gets past all of that,
+		 * we make an if statment to see if msg is 0. If it is, that means the username was not taken, so we do the same thing
+		 * for the email. If the email is taken, we set msg = 2 so the JavaScript file knows what ot print out. If the email is not
+		 * taken, we make msg = 0 like before. 
+		 */
 		ArrayList<User> allUsers = (ArrayList<User>) userRepo.findAll();
-		
 		for(int i = 0; i < allUsers.size(); i++) {
 			if(allUsers.get(i).getUsername().equals(allInfo.get(0))) {
 				msg = 1;
@@ -145,6 +164,13 @@ public class SitrusController {
 			}
 		}
 		
+		
+		/* Take all the information that has been passed in, and go through it 1 by one to create our user. 
+		 * The information passed in will always be in the same order, so we can set our user object like 
+		 * we do below. However, we only do the below steps if msg == 0 because if it is not 0, that means that we found out
+		 * above that the user already exists, so we will not create another user with the same
+		 * username or email.
+		 */
 		if(msg == 0) {
 		
 			enteredUser.setUsername(allInfo.get(0));
